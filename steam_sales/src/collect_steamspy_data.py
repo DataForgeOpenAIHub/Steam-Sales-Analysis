b89af464c214a9079edaa5da7f6f5cb64d894915
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-06-24 20:41:15 -0700

    chore: Refactor steam data collection and parsing logic, improve rate limiting handling, and update SQL query

--- steam_sales/src/collect_steamspy_data.py
+++ steam_sales/src/collect_steamspy_data.py
@@ -2,7 +2,7 @@ import os
 from multiprocessing import Pool, cpu_count
 
 from collect_metadata import get_request
-from crud import bulk_ingest_data
+from crud import bulk_ingest_steamspy_data
 from db import get_db
 from settings import Path, config, get_logger
 from sqlalchemy import text
@@ -16,6 +16,7 @@ def parse_steamspy_request(appid: int):
     url = config.STEAMSPY_BASE_URL
     parameters = {"request": "appdetails", "appid": appid}
     json_data = get_request(url, parameters)
+
     return GameDetails(**json_data)
 
 
@@ -24,12 +25,13 @@ def fetch_and_process_app_data(app_id_list):
     with Pool(processes=cpu_count()) as pool:
         results = pool.map(parse_steamspy_request, app_id_list)
         app_data.extend(filter(None, results))
+
     return GameDetailsList(games=app_data)
 
 
 def main():
     db = get_db()
-    with open(os.path.join(Path.sql_queries, "appid_dup.sql"), "r") as f:
+    with open(os.path.join(Path.sql_queries, "steamspy_appid_dup.sql"), "r") as f:
         query = text(f.read())
 
     result = db.execute(query)
@@ -45,7 +47,7 @@ def main():
         batch = app_id_list[i : i + batch_size]
         app_data = fetch_and_process_app_data(batch)
 
-        bulk_ingest_data(app_data, db)
+        bulk_ingest_steamspy_data(app_data, db)
         logger.info(f"Batch {current_batch} of {num_batches} completed")
 
 
