48e7ceacc2d587b079c1b442124bef03d0d18bc3
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-06-24 20:41:10 -0700

    Refactor steam data collection and parsing logic

--- steam_sales/src/collect_steam_data.py
+++ steam_sales/src/collect_steam_data.py
@@ -1,17 +1,24 @@
+import os
+from multiprocessing import Pool, cpu_count
+
+from bs4 import BeautifulSoup
 from collect_metadata import get_request
-from rich.pretty import pretty_repr
-from settings import config, get_logger
-from validation import Game
+from crud import bulk_ingest_steam_data
+from db import get_db
+from settings import Path, config, get_logger
+from sqlalchemy import text
+from validation import Game, GameList
 
 logger = get_logger(__name__)
 
 
-def steam_request(appid: int):
+def parse_steam_request(appid: int):
     url = f"{config.STEAM_BASE_SEARCH_URL}/api/appdetails/"
     parameters = {"appids": appid}
 
     json_data = get_request(url, parameters=parameters)
-    json_app_data = json_data[str(appid)]
+    if json_data:
+        json_app_data = json_data[str(appid)]
 
     if json_app_data["success"]:
         data = json_app_data["data"]
@@ -20,42 +27,112 @@ def steam_request(appid: int):
     return None
 
 
-def parse_game_data(data: dict):
-    game_data = {
-        "appid": data["steam_appid"],
-        "name": data["name"],
-        "type": data["type"],
-        "required_age": data["required_age"],
-        "is_free": data["is_free"],
-        "controller_support": data.get("controller_support", None),
-        "dlc": data.get("dlc", []),
-        "detailed_description": data["detailed_description"],
-        "short_description": data["short_description"],
-        "about_the_game": data["about_the_game"],
-        "supported_languages": data["supported_languages"],
-        "reviews": data["reviews"],
-        "header_image": data["header_image"],
-        "capsule_image": data["capsule_image"],
-        "website": data["website"],
-        "pc_requirements": data["pc_requirements"],
-        "mac_requirements": data["mac_requirements"],
-        "linux_requirements": data["linux_requirements"],
-        "developers": data["developers"],
-        "publishers": data["publishers"],
-        "platforms": data["platforms"],
-        "metacritic": data["metacritic"]["score"],
-        "categories": data["categories"],
-        "genres": data["genres"],
-        "recommendations": data["recommendations"]["total"],
-        "achievements": data["achievements"]["total"],
-        "release_date": data["release_date"],
-    }
+def parse_html_to_dict(html_content: str):
+    soup = BeautifulSoup(html_content, "lxml")
+    plain_text = soup.get_text(separator="\n", strip=True)
+    lines = plain_text.split("\n")
+    requirements_dict = {}
 
-    return Game(**game_data)
+    for i in range(0, len(lines) - 1, 2):
+        requirements_dict[lines[i]] = lines[i + 1]
+
+    return requirements_dict
+
+
+def requirements_parser(requirements: dict):
+    requirements_dict = {"minimum": None, "recommended": None}
+
+    if "minimum" in requirements:
+        requirements_dict["minimum"] = parse_html_to_dict(requirements["minimum"])
+
+    if "recommended" in requirements:
+        requirements_dict["recommended"] = parse_html_to_dict(requirements["recommended"])
+
+    return requirements_dict
+
+
+def text_parser(text: str):
+    if text:
+        soup = BeautifulSoup(text, "lxml")
+        plain_text = soup.get_text(separator="\n", strip=True)
+        return plain_text
+
+
+def parse_game_data(data: dict):
+    try:
+        if "English" not in data["supported_languages"]:
+            return None
+
+        game_data = {
+            "appid": data["steam_appid"],
+            "name": data["name"],
+            "type": data["type"],
+            "required_age": data["required_age"],
+            "is_free": data["is_free"],
+            "controller_support": data.get("controller_support", None),
+            "dlc": data.get("dlc", []),
+            "detailed_description": text_parser(data["detailed_description"]),
+            "short_description": text_parser(data["short_description"]),
+            "about_the_game": text_parser(data["about_the_game"]),
+            "supported_languages": text_parser(data["supported_languages"]),
+            "reviews": text_parser(data.get("reviews", None)),
+            "header_image": data["header_image"],
+            "capsule_image": data["capsule_image"],
+            "website": data.get("website", ""),
+            "pc_requirements": requirements_parser(data["pc_requirements"]),
+            "developers": data.get("developers", None),
+            "publishers": data["publishers"],
+            "pc_platform": data["platforms"]["windows"],
+            "metacritic": data.get("metacritic", {}).get("score", 0),
+            "categories": data.get("categories", None),
+            "genres": data.get("genres", None),
+            "recommendations": data.get("recommendations", {}).get("total", 0),
+            "achievements": data.get("achievements", {}).get("total", 0),
+            "release_date": data["release_date"]["date"],
+            "coming_soon": data["release_date"]["coming_soon"],
+        }
+        # logger.info(game_data)
+        return Game(**game_data)
+    except KeyError as ke:
+        logger.error(f"KeyError parsing game data for `{data['steam_appid']}`: Missing key {ke}")
+    except Exception as e:
+        logger.error(f"Error parsing game data for `{data.get('steam_appid', 'unknown')}`: {str(e)}")
+
+    return None
+
+
+def fetch_and_process_app_data(app_id_list):
+    app_data = []
+    with Pool(processes=cpu_count()) as pool:
+        results = pool.map(parse_steam_request, app_id_list)
+        app_data.extend(filter(None, results))
+
+    final_app_data = [game for game in (parse_game_data(data) for data in app_data) if game is not None]
+
+    return GameList(games=final_app_data)
+
+
+def main():
+    db = get_db()
+    with open(os.path.join(Path.sql_queries, "steam_appid_dup.sql"), "r") as f:
+        query = text(f.read())
+
+    result = db.execute(query)
+    app_id_list = [row[0] for row in result.fetchall()]
+    logger.info(f"{len(app_id_list)} ID's found")
+
+    batch_size = 5
+    num_batches = (len(app_id_list) + batch_size - 1) // batch_size
+    current_batch = 0
+
+    for i in range(0, len(app_id_list), batch_size):
+        current_batch += 1
+        batch = app_id_list[i : i + batch_size]
+        app_data = fetch_and_process_app_data(batch)
+
+        bulk_ingest_steam_data(app_data, db)
+        logger.info(f"Batch {current_batch} of {num_batches} completed")
 
 
 if __name__ == "__main__":
-    appid = 1172470
-    data = steam_request(appid)
-    game_data = parse_game_data(data)
-    logger.info(pretty_repr(game_data))
+    main()
