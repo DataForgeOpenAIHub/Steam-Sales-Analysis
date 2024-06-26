6df3e6a35c16f791877f8e9d48aed8699218e5db
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-06-26 00:40:21 -0700

    Refactor steam data collection logic for better efficiency and error handling

--- steam_sales/src/collect_steamspy_data.py
+++ steam_sales/src/collect_steamspy_data.py
@@ -13,6 +13,15 @@ logger = get_logger(__file__)
 
 
 def parse_steamspy_request(appid: int):
+    """
+    Parses the SteamSpy request for a specific app ID.
+
+    Args:
+        appid (int): The ID of the app to retrieve details for.
+
+    Returns:
+        GameDetails: An instance of the GameDetails class containing the parsed data.
+    """
     url = config.STEAMSPY_BASE_URL
     parameters = {"request": "appdetails", "appid": appid}
     json_data = get_request(url, parameters)
@@ -21,6 +30,16 @@ def parse_steamspy_request(appid: int):
 
 
 def fetch_and_process_app_data(app_id_list):
+    """
+    Fetches and processes app data for a given list of app IDs.
+
+    Args:
+        app_id_list (list): A list of app IDs to fetch data for.
+
+    Returns:
+        GameDetailsList: A list of game details objects containing the fetched app data.
+    """
+
     app_data = []
     with Pool(processes=cpu_count()) as pool:
         results = pool.map(parse_steamspy_request, app_id_list)
