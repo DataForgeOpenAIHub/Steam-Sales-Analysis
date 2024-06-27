64764b501c7b4f6649b31951dd45f2bb491d24ab
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2024-06-26 18:48:02 -0700

    Refactor SQL queries to remove duplicate entries and order by appid

--- steam_sales/sql/steam_appid_dup.sql
+++ steam_sales/sql/steam_appid_dup.sql
@@ -3,4 +3,6 @@ FROM steamspy_games_metadata
 WHERE appid NOT IN (
         SELECT appid
         FROM steam_games_raw
-    );
+    )
+ORDER BY
+    appid ASC;
--- steam_sales/sql/steamspy_appid_dup.sql
+++ steam_sales/sql/steamspy_appid_dup.sql
@@ -3,4 +3,6 @@ FROM steamspy_games_metadata
 WHERE appid NOT IN (
         SELECT appid
         FROM steamspy_games_raw
-    );
+    )
+ORDER BY
+    appid ASC;
