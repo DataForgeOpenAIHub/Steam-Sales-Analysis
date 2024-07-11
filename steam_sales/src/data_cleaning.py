62b508b7ab383ccb1c24c4c3104d0d8ea7f5d992
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-07-11 13:13:02 -0700

    added data cleaning functions

---
+++ steam_sales/src/data_cleaning.py
@@ -0,0 +1,565 @@
+import json
+import os
+import warnings
+from ast import literal_eval
+
+import dateparser
+import pandas as pd
+from bs4 import BeautifulSoup
+from db import get_db
+from settings import Path, get_logger
+from sqlalchemy import text
+
+warnings.filterwarnings("ignore")
+
+logger = get_logger(__file__)
+
+
+def fetch_data(source: str):
+    """
+    Fetches data from a specified source and returns it as a pandas DataFrame.
+
+    Parameters:
+    source (str): The name of the source file containing the SQL query.
+
+    Returns:
+    pandas.DataFrame: The fetched data as a DataFrame.
+
+    """
+    db = get_db()
+
+    with open(os.path.join(Path.sql_queries, source), "r") as f:
+        query = text(f.read())
+
+    result = db.execute(query)
+    data = result.fetchall()
+    columns = result.keys()
+    df = pd.DataFrame(data, columns=columns)
+
+    db.close()
+
+    return df
+
+
+def process_null(df):
+    """
+    Process null values in a DataFrame by replacing specific values with None.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to process.
+
+    Returns:
+        pandas.DataFrame: The processed DataFrame with null values replaced.
+
+    """
+    df = df.copy()
+
+    convert_to_none = ["", " ", "None", "none", "null", "N/a", "n/a", "N/A", "NA", '["none"]', '["null"]', "{}"]
+    df.replace(convert_to_none, None, inplace=True)
+
+    return df
+
+
+def process_age(df):
+    """
+    Process the 'required_age' column in the given DataFrame.
+
+    Parameters:
+    df (DataFrame): The input DataFrame containing the 'required_age' column.
+
+    Returns:
+    DataFrame: A copy of the input DataFrame with the 'required_age' column processed.
+
+    """
+    df = df.copy()
+    cut_points = [-1, 0, 3, 7, 12, 16, 1000]
+    categories = [0, 3, 7, 12, 16, 18]
+
+    df = df[df["required_age"].notna()]
+    df["required_age"] = pd.cut(df["required_age"], bins=cut_points, labels=categories)
+
+    return df
+
+
+def process_platforms(df):
+    """
+    Process the platforms column in the given DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the platforms column.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the platforms column processed.
+    """
+    df = df.copy()
+
+    def parse_platforms(x):
+        """
+        Parse the platforms from the given JSON string.
+
+        Args:
+            x (str): The JSON string representing the platforms.
+
+        Returns:
+            str: The parsed platforms separated by semicolons.
+        """
+        d = json.loads(x)
+
+        return ";".join(platform for platform in d.keys() if d[platform])
+
+    df["platform"] = df["platform"].apply(parse_platforms)
+
+    return df
+
+
+def process_language(df):
+    """
+    Process the language data in the given DataFrame.
+
+    Parameters:
+    df (DataFrame): The input DataFrame containing the language data.
+
+    Returns:
+    DataFrame: The processed DataFrame with the language data cleaned.
+
+    """
+    df = df.copy()
+
+    df = df.dropna(subset=["supported_languages"])
+
+    df["english"] = df["supported_languages"].apply(lambda x: 1 if "english" in x.lower() else 0)
+    df = df.drop("supported_languages", axis=1)
+
+    return df
+
+
+def process_developers_and_publishers(df):
+    """
+    Process the developers and publishers columns in the given DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the developers and publishers columns.
+
+    Returns:
+        pandas.DataFrame: The processed DataFrame with the developers and publishers columns transformed.
+
+    """
+    pattern = r'(?i)\["(n/a|na|null)"\]'
+
+    df = df[
+        (df["developers"].notna())
+        & (df["publishers"].notna() & (df["publishers"] != '[""]') & (df["publishers"] != '[" "]'))
+    ]
+    df = df[~df["developers"].str.contains(pattern, na=False)]
+    df = df[~df["publishers"].str.contains(pattern, na=False)]
+
+    df = df[~df["developers"].str.contains(";", na=False)]
+    df = df[~df["publishers"].str.contains(";", na=False)]
+
+    def safe_literal_eval(val):
+        try:
+            result = literal_eval(val)
+            if isinstance(result, list):
+                return ";".join(filter(None, result))
+        except (ValueError, SyntaxError):
+            return ""
+        return val
+
+    df["developer"] = df["developers"].apply(safe_literal_eval)
+    df["publisher"] = df["publishers"].apply(safe_literal_eval)
+
+    df = df.drop(["developers", "publishers"], axis=1)
+
+    return df
+
+
+def process_price(df):
+    """
+    Process the price data in the given DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the price data.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with processed price data.
+    """
+    df = df.copy()
+    currency_rates = {"EUR": 1.08, "TWD": 0.03, "SGD": 0.74, "BRL": 0.18, "AUD": 0.67}
+
+    def parse_price(x):
+        """
+        Parses the price information from a string representation.
+
+        Parameters:
+        x (str): The string representation of the price.
+
+        Returns:
+        dict: A dictionary containing the parsed price information. The dictionary has two keys:
+              - 'currency': The currency of the price.
+              - 'initial': The initial price value.
+        """
+        if x is not None:
+            return literal_eval(x)
+        else:
+            return {"currency": "USD", "initial": -1}
+
+    def convert_to_usd(price, currency, rates):
+        """
+        Converts the given price from the specified currency to USD using the provided exchange rates.
+
+        Parameters:
+        price (float): The price to be converted.
+        currency (str): The currency of the price.
+        rates (dict): A dictionary containing exchange rates for different currencies.
+
+        Returns:
+        float: The converted price in USD.
+        """
+        if currency == "USD":
+            return price
+
+        currency_rate = rates[currency]
+        return price * currency_rate
+
+    df["price_overview"] = df["price_overview"].apply(parse_price)
+
+    df["currency"] = df["price_overview"].apply(lambda x: x["currency"])
+    df["price"] = df["price_overview"].apply(lambda x: x["initial"])
+
+    df.loc[df["is_free"] == 1, "price"] = 0
+    df.loc[df["price"] > 0, "price"] /= 100
+
+    df["price"] = df.apply(lambda x: convert_to_usd(x["price"], x["currency"], currency_rates), axis=1)
+
+    df = df.drop(["is_free", "currency", "price_overview"], axis=1)
+
+    return df
+
+
+def process_categories_and_genres(df):
+    """
+    Process the categories and genres columns of the given DataFrame.
+
+    Parameters:
+    df (DataFrame): The input DataFrame containing the 'categories' and 'genres' columns.
+
+    Returns:
+    DataFrame: The processed DataFrame with the 'categories' and 'genres' columns modified.
+    """
+    df = df.copy()
+    df = df[(df["categories"].notna()) & (df["genres"].notna())]
+
+    for col in ["categories", "genres"]:
+        df[col] = df[col].apply(lambda x: ";".join(item["description"] for item in literal_eval(x)))
+
+    return df
+
+
+def process_controller(df):
+    """
+    Process the controller support column in the given DataFrame.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame containing the controller support column.
+
+    Returns:
+    pandas.DataFrame: The DataFrame with the controller support column processed.
+    """
+    df = df.copy()
+    df["controller_support"] = df["controller_support"].apply(lambda x: 1 if x == "full" else 0)
+
+    return df
+
+
+def process_dlc(df):
+    """
+    Process the 'dlc' column in the given DataFrame by converting the values to the number of DLCs available for each game.
+
+    Args:
+        df (DataFrame): The input DataFrame containing the 'dlc' column.
+
+    Returns:
+        DataFrame: A copy of the input DataFrame with the 'dlc' column transformed to the number of DLCs.
+
+    """
+    df = df.copy()
+
+    def parse_list(x):
+        """
+        Parses a string representation of a list and returns the length of the list.
+
+        Parameters:
+        x (str): A string representation of a list.
+
+        Returns:
+        int: The length of the list.
+
+        """
+        lst = literal_eval(x)
+        return len(lst)
+
+    df["dlc"] = df["dlc"].apply(parse_list)
+
+    return df
+
+
+def process_requirement(df):
+    """
+    Process the requirements column of the given DataFrame.
+
+    Parameters:
+    df (DataFrame): The input DataFrame containing the requirements column.
+
+    Returns:
+    DataFrame: A copy of the input DataFrame with the requirements column processed.
+
+    """
+    df = df.copy()
+    df["requirements"] = df["requirements"].apply(
+        lambda x: BeautifulSoup(x, "lxml").get_text() if x else "Not available"
+    )
+
+    return df
+
+
+def process_date(df):
+    """
+    Process the release date column in the given DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the release date column.
+
+    Returns:
+        pandas.DataFrame: The DataFrame with the release date column processed.
+
+    """
+    df = df.copy()
+
+    def parse_date(date_string):
+        """
+        Parses a date string and returns a pandas datetime object.
+
+        Parameters:
+        date_string (str): The date string to be parsed.
+
+        Returns:
+        pd.Timestamp: A pandas datetime object representing the parsed date.
+
+        """
+        if isinstance(date_string, str):
+            try:
+                date_obj = dateparser.parse(date_string)
+                return pd.to_datetime(date_obj)
+
+            except:
+                date_str = date_string.replace(" ", "")
+                date_str = dateparser.parse(date_str)
+
+                return pd.to_datetime(date_str)
+
+        return pd.NaT
+
+    df["release_date"] = df["release_date"].apply(parse_date)
+
+    return df
+
+
+def process_descriptions(df):
+    """
+    Process the descriptions of a DataFrame by combining multiple columns and performing some cleaning.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame containing the columns to be processed.
+
+    Returns:
+        pandas.DataFrame: The processed DataFrame with unnecessary columns dropped.
+    """
+    df = df.copy()
+
+    df["description"] = (
+        df["detailed_description"].fillna("")
+        + " "
+        + df["about_the_game"].fillna("")
+        + " "
+        + df["short_description"].fillna("")
+        + " Website: "
+        + df["website"].fillna("Not available")
+        + " Game Image: "
+        + df["header_image"].fillna("Not available")
+    )
+    df["description"] = df["description"].replace("", "Not available")
+    df = df.drop(["detailed_description", "about_the_game", "short_description", "website", "header_image"], axis=1)
+
+    return df
+
+
+def misc(df):
+    """
+    Perform miscellaneous data cleaning operations on the given DataFrame.
+
+    Parameters:
+    df (pandas.DataFrame): The input DataFrame to be cleaned.
+
+    Returns:
+    pandas.DataFrame: The cleaned DataFrame.
+    """
+    df = df.copy()
+    col_to_drop = ["capsule_image", "reviews"]
+
+    df["year"] = df["release_date"].dt.year.astype("Int16")
+    df["month"] = df["release_date"].dt.month.astype("Int16")
+    df["day"] = df["release_date"].dt.day.astype("Int16")
+
+    df = df.drop(col_to_drop, axis=1)
+
+    return df
+
+
+def process_col_rows(df):
+    df = df.copy()
+    col_to_drop = [
+        "score_rank",  # too many missing values
+        "average_forever",
+        "median_forever",  # too little variance (most have 0)
+        "userscore",  # too little variance (most have 0)
+        "genre",
+        "developer",
+        "publisher",
+        "price",
+        "initialprice",
+        "discount",  # provided by Steam data
+        "average_2weeks",
+        "median_2weeks",
+        "ccu",  # not interested in temporally specific columns
+    ]
+
+    # Drop missing games
+    df = df.dropna(subset=["name"])
+    df = df.drop(col_to_drop, axis=1)
+
+    return df
+
+
+def process_owners(df):
+    """
+    Process the 'owners' column in the given DataFrame.
+
+    Parameters:
+    df (pandas.DataFrame): The DataFrame containing the 'owners' column.
+
+    Returns:
+    pandas.DataFrame: The DataFrame with the 'owners' column processed.
+
+    """
+    df = df.copy()
+    df["owners"] = (
+        df["owners"]
+        .apply(lambda x: tuple(map(lambda x: int(x) / 1000000, x.replace(",", "").split(" .. "))))
+        .apply(lambda x: f"{x[0]} - {x[1]}")
+    )
+
+    return df
+
+
+def process_tag_lang(df):
+    """
+    Process the dataframe by dropping rows with missing values in the 'languages' and 'tags' columns.
+
+    Args:
+        df (pandas.DataFrame): The input dataframe.
+
+    Returns:
+        pandas.DataFrame: The processed dataframe with missing values dropped.
+
+    """
+    df = df.copy()
+    df = df.dropna(subset=["languages", "tags"])
+
+    return df
+
+
+def rename(df):
+    """
+    Renames the columns of the given DataFrame.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to be renamed.
+
+    Returns:
+        pandas.DataFrame: The renamed DataFrame.
+    """
+    df = df.copy()
+    df = df.rename(
+        {
+            "tags": "steamspy_tags",
+            "positive": "positive_ratings",
+            "negative": "negative_ratings",
+            "owners": "owners_in_millions",
+        },
+        axis=1,
+    )
+
+    return df
+
+
+def process_steamspy(df):
+    """
+    Process the given DataFrame by applying a series of cleaning operations.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to be processed.
+
+    Returns:
+        pandas.DataFrame: The processed DataFrame.
+
+    """
+    df = process_null(df)
+    df = process_col_rows(df)
+    df = process_owners(df)
+    df = process_tag_lang(df)
+    df = rename(df)
+
+    return df
+
+
+def process_steam(df):
+    """
+    Process the given DataFrame by applying a series of cleaning operations.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to be processed.
+
+    Returns:
+        pandas.DataFrame: The processed DataFrame.
+
+    """
+    df = process_null(df)
+    df = process_age(df)
+    df = process_platforms(df)
+    df = process_language(df)
+    df = process_developers_and_publishers(df)
+    df = process_price(df)
+    df = process_categories_and_genres(df)
+    df = process_controller(df)
+    df = process_dlc(df)
+    df = process_requirement(df)
+    df = process_date(df)
+    df = process_descriptions(df)
+    df = misc(df)
+
+    return df
+
+
+def main():
+    steam_data = fetch_data("get_all_steam_data.sql")
+    steamspy_data = fetch_data("get_all_steamspy_data.sql")
+
+    clean_steam_data = process_steam(steam_data)
+    clean_steamspy_data = process_steamspy(steamspy_data)
+
+    logger.info("Clean steam data shape: ", clean_steam_data.shape)
+    logger.info("Clean steamspy data shape: ", clean_steamspy_data.shape)
+
+
+if __name__ == "__main__":
+    main()
