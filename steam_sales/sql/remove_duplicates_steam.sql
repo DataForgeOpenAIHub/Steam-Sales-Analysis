5b7697b9356969ae6e97ab01662fedf169087ba7
Rohit Veeradhi <rohit.veeradhi@gmail.com>
2024-07-16 20:23:30 -0700

    removed

--- steam_sales/sql/remove_duplicates_steam.sql
+++
@@ -1,9 +0,0 @@
-DELETE FROM SteamSales.steam_games_raw
-WHERE id NOT IN (
-        SELECT *
-        FROM (
-                SELECT MIN(id)
-                FROM SteamSales.steam_games_raw
-                GROUP BY appid
-            ) AS temp_table
-    );
