15d94f65b75ea5eabd78ca4f87431e1fea46a909
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2024-08-11 01:01:03 -0700

    chore: Update project URL in setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ def parse_requirements(filename):
 setup(
     name="steam_sales",  # Name of your package
     version="0.1.0",  # Initial release version
+    author="DataForgeOpenAIHub",
     packages=find_packages(),  # Automatically find and include all packages
     include_package_data=True,  # Include data files specified in MANIFEST.in
     install_requires=parse_requirements("requirements.txt"),  # Use requirements.txt
@@ -21,7 +22,7 @@ setup(
     description="CLI for Steam Store Data Ingestion ETL Pipeline",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/steam_sales",  # Project URL
+    url="https://github.com/DataForgeOpenAIHub/Steam-Sales-Analysis",  # Project URL
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
