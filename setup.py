87044f0af0d9915dbaa7cbb97bf581cf0a1e7a47
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2024-08-12 01:04:37 -0700

    chore: Update setup.py to remove author field and update Python version requirement

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     name="steamstore_etl",  # Name of your package
     version=versioneer.get_version(),  # Initial release version
     cmdclass=versioneer.get_cmdclass(),  # Command line version
-    author="DataForgeOpenAIHub",
     packages=find_packages(),  # Automatically find and include all packages
     include_package_data=True,  # Include data files specified in MANIFEST.in
     install_requires=parse_requirements(
@@ -26,17 +25,5 @@ setup(
             "steamstore=steam_sales.app:app",
         ],
     },
-    license="MIT",  # License
-    description="CLI for Steam Store Data Ingestion ETL Pipeline",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/DataForgeOpenAIHub/Steam-Sales-Analysis",  # Project URL
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires=">=3.10",  # Specify minimum Python version if needed
 )
