78da0bc3b3177585b8104345c2400131559cff4d
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2024-08-12 02:13:28 -0700

    chore: Update project metadata and classifiers in pyproject.toml and setup.cfg

---
+++ get_version.py
@@ -0,0 +1,8 @@
+import warnings
+import versioneer
+
+if __name__ == "__main__":
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        version = versioneer.get_version()
+        print(version)
